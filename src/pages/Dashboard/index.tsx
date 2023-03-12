638835726a143e391522149007a73faae43b0031
Premsai <premsai.kantikar@tifin.com>
2023-03-13 00:50:25 +0530

    added redirects file

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import React from "react";
 import BudgetChart from "../../components/BudgetChart";
 import ProjectCard from "../../components/ProjectCard";
